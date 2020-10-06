cd64fa685b101461c9fdc5c81310493e9d07e9dc
Joshua Carey <jdcarey128@gmail.com>
2020-10-06 16:34:42 -0600

    Rewrite shelters instance variable to AR object

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,7 +1,7 @@
 class SheltersController < ApplicationController
 
   def index
-    @shelters = ["shelter 1", "shelter 2"]
+    @shelters = Shelter.all
   end
 
   def new
@@ -15,7 +15,7 @@ class SheltersController < ApplicationController
       state: params[:shelter][:state],
       zip: params[:shelter][:zip],
       })
-      
+
     shelter.save
 
     redirect_to '/shelters'
