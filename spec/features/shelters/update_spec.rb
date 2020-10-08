4c7d52c60a2a52bbb093c0dcb9bb2c9746ba572f
Joshua Carey <jdcarey128@gmail.com>
2020-10-07 20:57:25 -0600

    Update update test

--- spec/features/shelters/update_spec.rb
+++ spec/features/shelters/update_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 describe "As a user" do
-  describe "when the user visits '/shelters/:id/edit'" do
-    xit "the user sees a pre-filled form with editable text fields for the shelter's name, address, city, state, and zip" do
+  describe "when the user visits '/shelters/:id/edit' and clicks 'Update Shelter'" do
+    it "the user is redirected to the 'shelters/:id' page and sees the updated information" do
 
       shelter_1 = Shelter.create(name: "Colorado Cares", address: "867 magnolia st", city: "Lakewood", state: "CO", zip: "80022")
 
       visit "/shelters/#{shelter_1.id}/edit"
 
-      expect(page).to have_content(shelter_1.name)
-      expect(page).to have_content(shelter_1.address)
-      expect(page).to have_content(shelter_1.city)
-      expect(page).to have_content(shelter_1.state)
-      expect(page).to have_content(shelter_1.zip)
+      save_and_open_page
+      fill_in "shelter[name]", with: "Mile High Center"
+      fill_in "shelter[address]", with: "455 w first st"
+      fill_in "shelter[city]", with: "Denver"
 
+      # find('form["name=update"]').click
+
+      # expect(click_button "Update Shelter").to route_to("/shelters/#{shelter_1.id}")
+      expect(page).to have_content("Shelter ID: #{shelter_1.id}")
+      expect(page).to have_content("Name: Mile High Center")
+      expect(page).to have_content("Address: 455 w first st")
+      expect(page).to have_content("City/State/zip: Denver, CO 80022")
     end
-
-    xit "the user sees the updated '/shelters/:id' upon clicking the submit button" do
-
-    end
-
   end
 end
