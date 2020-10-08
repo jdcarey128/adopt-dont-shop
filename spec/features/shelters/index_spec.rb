e0449c06efde637ecfc721ec6960a543a4be0da9
Joshua Carey <jdcarey128@gmail.com>
2020-10-07 18:32:17 -0600

    Add 'New Shelter' test to index

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 describe "As a user" do
   describe "when the user visits '/shelters'" do
-    it "the user sees the name of each shelter in the system" do
+    it "the user sees the name of each shelter in the system and the user sees a 'New Shelter' link" do
 
       shelter_1 = Shelter.create(name: "Colorado Cares", address: "867 magnolia st", city: "Lakewood", state: "CO", zip: "80022")
       shelter_2 = Shelter.create(name: "Rocky Mountain High", address: "1234 fake st.", city: "Denver", state: "CO", zip: "45505")
 
       visit '/shelters'
-      
+
       expect(page).to have_link(shelter_1.name)
       expect(page).to have_link(shelter_2.name)
+      expect(page).to have_link("New Shelter")
     end
+
   end
 end
