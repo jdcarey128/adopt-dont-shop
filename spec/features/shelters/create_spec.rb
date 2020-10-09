949c535261893ae4cc768b29e3b147e4b9ef81a5
Joshua Carey <jdcarey128@gmail.com>
2020-10-08 18:50:17 -0600

    Revise blank new form test and reroute view

--- spec/features/shelters/create_spec.rb
+++ spec/features/shelters/create_spec.rb
@@ -2,19 +2,30 @@ require 'rails_helper'
 
 describe "As a user" do
   describe "when the user visits '/shelters/new'" do
-    it "the user sees a form to complete the shelter's name, address, city, state, and zip do" do
+    it "the user sees an empty form to complete the shelter's name, address, city, state, and zip do" do
 
       visit '/shelters/new'
 
       expect(page).to have_content("Name")
+      expect(find_field("shelter[name]").value).to eq(nil)
+
       expect(page).to have_content("Address")
+      expect(find_field("shelter[address]").value).to eq(nil)
+
       expect(page).to have_content("City")
+      expect(find_field("shelter[city]").value).to eq(nil)
+
       expect(page).to have_content("State")
+      expect(find_field("shelter[state]").value).to eq(nil)
+
       expect(page).to have_content("Zip")
+      expect(find_field("shelter[zip]").value).to eq(nil)
+
     end
 
-    xit "the user submits a new form and sees the new shelter on '/shelters'" do
+    it "the user submits a new form and sees the new shelter on '/shelters'" do
       visit '/shelters/new'
+
       fill_in "shelter[name]", with: "Mile High Cares"
       fill_in "shelter[address]", with: "4550 north st"
       fill_in "shelter[city]", with: "Denver"
@@ -23,11 +34,7 @@ describe "As a user" do
 
       click_button("Create Shelter")
       expect(page).to have_content("Mile High Cares")
-      expect(page).to have_content("4550 north st")
-      expect(page).to have_content("Denver")
-      expect(page).to have_content("CO")
-      expect(page).to have_content("80219")
-
+      
     end
 
 
