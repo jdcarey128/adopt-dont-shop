d07e266bc163eb1d80f46696557ff1c8bafeb2ec
Joshua Carey <jdcarey128@gmail.com>
2020-10-08 19:14:16 -0600

    Move destroy test to show and delete destroy file

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "As a user" do
   describe "when the user visits '/shelters/:id'" do
-    it "the user sees the shelter's name, address, city, state, and zip and sees a link to 'Update Shelter'" do
+    it "the user sees the shelter's name, address, city, state, and zip and sees a link to 'Update Shelter' and 'Delete Shelter'" do
 
       shelter_1 = Shelter.create(name: "Colorado Cares", address: "867 magnolia st", city: "Lakewood", state: "CO", zip: "80022")
 
@@ -14,6 +14,7 @@ describe "As a user" do
       expect(page).to have_content(shelter_1.state)
       expect(page).to have_content(shelter_1.zip)
       expect(page).to have_button("Update Shelter")
+      expect(page).to have_button("Delete Shelter")
     end
 
     it "the user is taken to '/shelters/:id/edit' when they click 'Update Shelter'" do
@@ -24,6 +25,18 @@ describe "As a user" do
       expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
       # below looks at url params
       # expect(page).to have_current_path("/shelters/#{shelter_1.id}/edit")
+    end
+
+    it "the user is taken to '/shelters' when they click 'Delete Shelter' and the shelter is not listed in index" do
+      shelter_1 = Shelter.create(name: "Colorado Cares", address: "867 magnolia st", city: "Lakewood", state: "CO", zip: "80022")
+      shelter_2 = Shelter.create(name: "Rocky Mountain High", address: "1234 fake st.", city: "Denver", state: "CO", zip: "45505")
+
+      visit "/shelters/#{shelter_1.id}"
+
+      click_button("Delete Shelter")
+      expect(current_path).to eq("/shelters")
+      expect(page).to_not have_content("Colorado Cares")
+      expect(page).to have_content("Rocky Mountain High")
     end
 
   end
