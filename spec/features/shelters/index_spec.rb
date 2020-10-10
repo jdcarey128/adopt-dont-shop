06af63492d279d73041251bba3db7ffd4db0f811
Joshua Carey <jdcarey128@gmail.com>
2020-10-10 13:54:13 -0600

    Add 'edit shelter' link for each shelter in the shelter index

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe "As a user" do
-  describe "when the user visits '/shelters'" do
-    it "the user sees the name of each shelter in the system and the user sees a 'New Shelter' link" do
+  describe "when I visit '/shelters'" do
+    it "I see the name of each shelter in the system and I see a 'New Shelter' link" do
 
       shelter_1 = Shelter.create(name: "Colorado Cares", address: "867 magnolia st", city: "Lakewood", state: "CO", zip: "80022")
       shelter_2 = Shelter.create(name: "Rocky Mountain High", address: "1234 fake st.", city: "Denver", state: "CO", zip: "45505")
@@ -14,11 +14,35 @@ describe "As a user" do
       expect(page).to have_link("New Shelter")
     end
 
-    it "the user is routed to '/shelters/new' when they click on the 'New Shelter' link" do
+    it "I am routed to '/shelters/new' when I click on the 'New Shelter' link" do
       visit '/shelters'
 
       click_link("New Shelter")
       expect(current_path).to eq('/shelters/new')
+    end
+
+    describe "user story 13" do
+      it "I can click on an 'edit shelter' link for each listed shelter" do
+        shelter_1 = Shelter.create(name: "Colorado Cares", address: "867 magnolia st", city: "Lakewood", state: "CO", zip: "80022")
+        shelter_2 = Shelter.create(name: "Rocky Mountain High", address: "1234 fake st.", city: "Denver", state: "CO", zip: "45505")
+        
+        visit "/shelters"
+
+        within("#shelter_#{shelter_1.id}") do
+          expect(page).to have_link("edit shelter")
+          click_link("edit shelter")
+          expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+        end
+
+        visit "/shelters"
+
+        within("#shelter_#{shelter_2.id}") do
+          expect(page).to have_link("edit shelter")
+          click_link("edit shelter")
+          expect(current_path).to eq("/shelters/#{shelter_2.id}/edit")
+        end
+
+      end
     end
 
   end
