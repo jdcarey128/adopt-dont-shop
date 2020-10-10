1e3e6da8e004871f63e65d554033485ce52b36bf
Joshua Carey <jdcarey128@gmail.com>
2020-10-10 06:49:34 -0600

    add default values for description and adoption status after initialize

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,15 @@
 class Pet < ApplicationRecord
-  belongs_to :shelter 
+  belongs_to :shelter
+  after_initialize :init
 
   validates_presence_of :image
   validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :adoption_status
+
+  def init
+    self.description ||= "This pet does not have a description."
+    self.adoption_status ||= true
+  end
 
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Pet do
   describe "validations" do
     it {should validate_presence_of :image}
     it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :adoption_status}
     it {should belong_to :shelter}
   end
 end
