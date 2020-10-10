3d3d27387f9a3fe87ad497428317cc4115de1494
Joshua Carey <jdcarey128@gmail.com>
2020-10-10 07:01:10 -0600

    Define show method and adoption_status class method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,4 +4,13 @@ class PetsController < ApplicationController
     @pets = Pet.all
   end
 
+  def show
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def self.adoption_status(status)
+    return "Adoptable" if status
+    return "Pending" if !status
+  end
+
 end
