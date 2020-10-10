bd797b842f68ad187bce78736440c0ca4a44e6ea
Joshua Carey <jdcarey128@gmail.com>
2020-10-10 07:59:22 -0600

    Add description validation back into pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,6 +4,7 @@ class Pet < ApplicationRecord
 
   validates_presence_of :image
   validates_presence_of :name
+  validates_presence_of :description
   validates_presence_of :adoption_status
 
   def init
