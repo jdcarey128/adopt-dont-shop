ecaf39aee94a391ebd0d15509f229e59f2a6950f
Joshua Carey <jdcarey128@gmail.com>
2020-10-10 07:52:17 -0600

    Modify show to pass

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -7,14 +7,40 @@ RSpec.describe "As a visitor" do
       pet_1 = shelter_1.pets.create(image: "https://dogtime.com/assets/uploads/gallery/austalian-shepherd-dog-breed-pictures/10-threequarters.jpg",
                         name: "Tony",
                         approximate_age: "2",
-                        sex: "male")
+                        sex: "male",
+                        adoption_status: "adoptable")
+      pet_2 = shelter_1.pets.create(image: "https://dogtime.com/assets/uploads/gallery/german-shorthaired-pointer-dogs-and-puppies/german-shorthaired-pointer-dogs-puppies-3.jpg",
+                                    name: "Isabell",
+                                    approximate_age: "5",
+                                    sex: "female",
+                                    adoption_status: "pending")
       visit "/pets/#{pet_1.id}"
 
       expect(page).to have_xpath("//img [contains(@src, '#{pet_1.image}')]")
       expect(page).to have_content(pet_1.name)
+      expect(page).to have_content(pet_1.description)
       expect(page).to have_content(pet_1.approximate_age)
       expect(page).to have_content(pet_1.sex)
-      expect(page).to have_content(pet_1.shelter.name)
+      expect(page).to have_content("adoptable")
+
+      expect(page).to_not have_content(pet_2.name)
+      expect(page).to_not have_content(pet_2.approximate_age)
+      expect(page).to_not have_content(pet_2.sex)
+      expect(page).to_not have_content("pending")
+
+      visit "/pets/#{pet_2.id}"
+
+      expect(page).to have_xpath("//img [contains(@src, '#{pet_2.image}')]")
+      expect(page).to have_content(pet_2.name)
+      expect(page).to have_content(pet_2.description)
+      expect(page).to have_content(pet_2.approximate_age)
+      expect(page).to have_content(pet_2.sex)
+      expect(page).to have_content("pending")
+
+      expect(page).to_not have_content(pet_1.name)
+      expect(page).to_not have_content(pet_1.approximate_age)
+      expect(page).to_not have_content(pet_1.sex, exact:true)
+      expect(page).to_not have_content("adoptable")
     end
   end
 end
